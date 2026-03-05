278f1e487ec78ab128c77d6dad30ae9610d3d734
XXJen <83561@example.com>
2026-03-06 00:40:58 +0800

    feat: 完成Hello接口开发并测试通过

---
+++ hello-spec/src/main/java/com/example/hellospec/HelloSpecApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellospec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSpecApplication.class, args);
+    }
+
+}
---
+++ hello-spec/src/test/java/com/example/hellospec/HelloSpecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellospec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpecApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
